27d9bc1b3d60eca14845bc2351ee1d2b65bf3d5c
Yasin Radi <yasin.ben.hamman@gmail.com>
2017-05-02 17:59:52 +0200

    Finished Full Data Router GET routes.

---
+++ routes/fulldata.js
@@ -0,0 +1,47 @@
+/**
+ * Created by YasinR on 02/05/2017.
+ */
+'use strict';
+
+let express = require('express');
+let router  = express.Router();
+const data = require('../lib/FullDataModel');
+
+//<editor-fold desc="Full Tables Data">
+
+/**
+ * GET All tables data using a given id.
+ */
+router.get('/id/:id', function(req, res, next) {
+    data.getAllDataByAccountId(res, req.params.id);
+});
+
+/**
+ * GET All tables data using a given email.
+ */
+router.get('/email/:email', function(req, res, next) {
+    data.getAllDataByEmail(res, req.params.email);
+});
+
+/**
+ * GET All tables data using a given name.
+ */
+router.get('/name/:name', function(req, res, next) {
+    data.getAllDataByName(res, req.params.name);
+});
+
+/**
+ * GET All tables data using a given surname.
+ */
+router.get('/surname/:surname', function(req, res, next) {
+    data.getAllDataBySurname(res, req.params.surname);
+});
+
+/**
+ * GET All tables data using a given username.
+ */
+router.get('/username/:username', function(req, res, next) {
+    data.getAllDataByUsername(res, req.params.username);
+});
+
+//</editor-fold>

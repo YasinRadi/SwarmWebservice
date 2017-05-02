bfe679c77357cec4816adc0158bb4135a8fe8d52
Yasin Radi <yasin.ben.hamman@gmail.com>
2017-05-02 19:52:46 +0200

    Finished basic user data form.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile('./index.html');
+    res.sendfile('./index.html');
+});
+
+/**
+ * GET form page.
+ */
+router.get('/form', function(req, res, next) {
+    res.sendfile('./public/form.html');
 });
 
 module.exports = router;

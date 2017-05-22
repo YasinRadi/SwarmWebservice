d98fbbd62505f4306e07c809977ab5738b52645b
Yasin Radi <yasin.ben.hamman@gmail.com>
2017-05-22 03:08:13 +0200

    feature/Jasmine: Jasmine setup expanded. Installed Jasmine asynch module. Setup spec/ directory, testSpec.js file and 4 unit-tests.

--- routes/accounts.js
+++ routes/accounts.js
@@ -20,42 +20,42 @@ router.use(fileUp());
  * GET all User Accounts.
  */
 router.get('/data', function(req, res, next) {
-    account.getAccountData(res);
+    account.getAccountData();
 });
 
 /**
  * GET Account data by id.
  */
 router.get('/data/id/:id', function(req, res, next) {
-    account.getAccountById(res, req.params.id);
+    account.getAccountById(req.params.id);
 });
 
 /**
  * GET Account data by email.
  */
 router.get('/data/email/:email', function(req, res, next) {
-    account.getAccountByEmail(res, req.params.email);
+    account.getAccountByEmail(req.params.email);
 });
 
 /**
  * GET Account data by name.
  */
 router.get('/data/name/:name', function(req, res, next) {
-    account.getAccountByName(res, req.params.name);
+    account.getAccountByName(req.params.name);
 });
 
 /**
  * GET Account data by surname.
  */
 router.get('/data/surname/:surname', function(req, res, next) {
-    account.getAccountBySurname(res, req.params.surname);
+    account.getAccountBySurname(req.params.surname);
 });
 
 /**
  * GET Account data by username.
  */
 router.get('/data/username/:username', function(req, res, next) {
-    account.getAccountByUsername(res, req.params.username);
+    account.getAccountByUsername(req.params.username);
 });
 
 //</editor-fold>
@@ -65,35 +65,37 @@ router.get('/data/username/:username', function(req, res, next) {
  * GET all User Emails.
  */
 router.get('/emails', function(req, res, next) {
-    account.getEmails(res);
+    account.getEmails((e) => {
+        res.send(e);
+    });
 });
 
 /**
  * GET User Emails by id.
  */
 router.get('/emails/id/:id', function(req, res, next) {
-    account.getEmailById(res, req.params.id);
+    account.getEmailById(req.params.id);
 });
 
 /**
  * GET User Emails by name.
  */
 router.get('/emails/name/:name', function(req, res, next) {
-    account.getEmailByName(res, req.params.name);
+    account.getEmailByName(req.params.name);
 });
 
 /**
  * GET User Emails by surname.
  */
 router.get('/emails/surname/:surname', function(req, res, next) {
-    account.getEmailBySurname(res, req.params.surname);
+    account.getEmailBySurname(req.params.surname);
 });
 
 /**
  * GET User Emails by username.
  */
 router.get('/emails/username/:username', function(req, res, next) {
-    account.getEmailByUsername(res, req.params.username);
+    account.getEmailByUsername(req.params.username);
 });
 
 //</editor-fold>
@@ -103,35 +105,35 @@ router.get('/emails/username/:username', function(req, res, next) {
  * GET all User Names.
  */
 router.get('/names', function(req, res, next) {
-    account.getNames(res);
+    account.getNames();
 });
 
 /**
  * GET all User Names by id.
  */
 router.get('/names/id/:id', function(req, res, next) {
-    account.getNameById(res, req.params.id);
+    account.getNameById(req.params.id);
 });
 
 /**
  * GET all User Names by surname.
  */
 router.get('/names/surname/:surname', function(req, res, next) {
-    account.getNameBySurname(res, req.params.surname);
+    account.getNameBySurname(req.params.surname);
 });
 
 /**
  * GET all User Names by username.
  */
 router.get('/names/username/:username', function(req, res, next) {
-    account.getNameByUsername(res, req.params.username);
+    account.getNameByUsername(req.params.username);
 });
 
 /**
  * GET all User Names by email.
  */
 router.get('/names/email/:email', function(req, res, next) {
-    account.getNameByEmail(res, req.params.email);
+    account.getNameByEmail(req.params.email);
 });
 
 //</editor-fold>
@@ -141,35 +143,35 @@ router.get('/names/email/:email', function(req, res, next) {
  * GET all User Surnames.
  */
 router.get('/surnames', function(req, res, next) {
-   account.getSurnames(res);
+   account.getSurnames();
 });
 
 /**
  * GET all User Surnames by id.
  */
 router.get('/surnames/id/:id', function(req, res, next) {
-    account.getSurnamesById(res, req.params.id);
+    account.getSurnamesById(req.params.id);
 });
 
 /**
  * GET all User Surnames by name.
  */
 router.get('/surnames/name/:name', function(req, res, next) {
-    account.getSurnamesByName(res, req.params.name);
+    account.getSurnamesByName(req.params.name);
 });
 
 /**
  * GET all User Surnames by email.
  */
 router.get('/surnames/email/:email', function(req, res, next) {
-    account.getSurnamesByEmail(res, req.params.email);
+    account.getSurnamesByEmail(req.params.email);
 });
 
 /**
  * GET all User Surnames by username.
  */
 router.get('/surnames/username/:username', function(req, res, next) {
-    account.getSurnamesByUsername(res, req.params.username);
+    account.getSurnamesByUsername(req.params.username);
 });
 
 //</editor-fold>
@@ -180,7 +182,7 @@ router.get('/surnames/username/:username', function(req, res, next) {
  * GET all User Full Names.
  */
 router.get('/fullnames', function(req, res, next) {
-   account.getFullNames(res);
+   account.getFullNames();
 });
 
 //</editor-fold>
@@ -191,36 +193,35 @@ router.get('/fullnames', function(req, res, next) {
  * GET all UserNames.
  */
 router.get('/usernames', function(req, res, next) {
-   account.getUserNames(res);
+   account.getUserNames();
 });
 
 /**
  * GET all UserNames by id.
  */
 router.get('/usernames/id/:id', function(req, res, next) {
-    console.log(req.params);
-    account.getUsernameById(res, req.params.id);
+    account.getUsernameById(req.params.id);
 });
 
 /**
  * GET all UserNames by name.
  */
 router.get('/usernames/name/:name', function(req, res, next) {
-    account.getUsernameByName(res, req.params.name);
+    account.getUsernameByName(req.params.name);
 });
 
 /**
  * GET all UserNames by surname.
  */
 router.get('/usernames/surname/:surname', function(req, res, next) {
-    account.getUsernameBySurname(res, req.params.surname);
+    account.getUsernameBySurname(req.params.surname);
 });
 
 /**
  * GET all UserNames by email.
  */
 router.get('/usernames/email/:email', function(req, res, next) {
-    account.getUsernameByEmail(res, req.params.email);
+    account.getUsernameByEmail(req.params.email);
 });
 
 //</editor-fold>
@@ -231,21 +232,21 @@ router.get('/usernames/email/:email', function(req, res, next) {
  * GET all Emails and Account id.
  */
 router.get('/emailids', function(req, res, next) {
-    account.getEmailsIds(res);
+    account.getEmailsIds();
 });
 
 /**
  * GET all Full Names and Account id.
  */
 router.get('/fullnameids', function(req, res, next) {
-    account.getFullnamesIds(res);
+    account.getFullnamesIds();
 });
 
 /**
  * GET all User-nams and Account id.
  */
 router.get('/usernameids', function(req, res, next) {
-    account.getUsernamesIds(res);
+    account.getUsernamesIds();
 });
 
 //</editor-fold>
